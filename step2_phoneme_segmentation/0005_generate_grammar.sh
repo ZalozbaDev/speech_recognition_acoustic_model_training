1a5afbc5e60b1434ee3918d3cdf5fa6e0f21dfbe
aStereoID <astrid.schmiedel@serbski-institut.de>
2024-06-28 10:11:24 +0200

    update corpus dsb

--- step2_phoneme_segmentation/0005_generate_grammar.sh
+++ step2_phoneme_segmentation/0005_generate_grammar.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 3 ]; then
 	echo "Please supply lexicon, labels and output file!"
-	echo "Example: ./0005_generate_grammar.sh inputs/grammar/dsb_lex.txt inputs/recordings/trl/ inputs/grammar/dsb.grm"
+	echo "Example: ./0005_generate_grammar.sh inputs/grammar/dsb_lex2.txt inputs/recordings/trl/ inputs/grammar/dsb.grm"
 	exit -1
 fi
 
