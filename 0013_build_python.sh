198bf3fa744c5f1b7b0bfc803dac2b39b0b5c9e0
Daniel Zoba <daniel.sobe@sorben.com>
2026-04-02 10:41:38 +0200

    build custom python

---
+++ 0013_build_python.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+
+if ! [ -e Python-3.10.0.tgz ] ; then
+	wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
+fi
+
+rm -rf Python-3.10.0
+tar xvfz Python-3.10.0.tgz
+
+cd Python-3.10.0 
+./configure --enable-optimizations 
+make -j8 
+echo "============================="
+echo "Now run: sudo make altinstall"
+
