142b1113da3130735352256400dfaa845ded93c7
Daniel Zoba <daniel.sobe@sorben.com>
2026-04-02 08:53:30 +0200

    adjust config again

--- 1020_run_corpus_creator.sh
+++ 1020_run_corpus_creator.sh
@@ -4,7 +4,7 @@ rm -rf generated/corpus_output/
 
 pushd dLabPro
 source bin/activate
-python3 ../corpus_creation/tooling/corpus_creator.py corpus_creation/configuration/DSB.yaml
+python3 ../corpus_creation/tooling/corpus_creator.py ../corpus_creation/configuration/DSB.yaml
 popd
 
 ls -lR generated/corpus_output/
