9af71f675a5a2f2e7ff61fd4286ec074a3744ae1
Daniel Zoba <daniel.sobe@sorben.com>
2026-04-02 08:45:20 +0200

    generate corpus from transcripts

---
+++ 1010_prepare_corpus_from_transcripts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Please supply folder to search for trl files!"
+	echo "Example: ./1010_and_so_on.sh recordings/trl/SPEAKERID/SESSION/"
+	exit -1
+fi
+
+TRANSCRIPTPATH=$1
+
+mkdir -p generated/corpus/
+
+CORPUS_OUT=generated/corpus/dsb_from_transcripts.vocab
+
+rm -f $CORPUS_OUT
+
+for i in $(find $TRANSCRIPTPATH -name "*.trl"); do
+	echo $i;
+	sed -e 's/\(.*\)/\U\1/' -e 's/\xef\xbb\xbf//' $i >> $CORPUS_OUT
+	echo -n " " >> $CORPUS_OUT
+done
