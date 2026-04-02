9d4ee901fe7025de0b166fccebfb4e74bd8ceb1f
Daniel Zoba <daniel.sobe@sorben.com>
2026-04-02 09:00:11 +0200

    update

--- 1020_run_corpus_creator.sh
+++ 1020_run_corpus_creator.sh
@@ -7,4 +7,4 @@ source bin/activate
 python3 ../corpus_creation/tooling/corpus_creator.py ../corpus_creation/configuration/DSB.yaml
 popd
 
-ls -lR generated/corpus_output/
+ls -lR generated/corpus_output/uasr_configurations/
