d690884ed7e4942a7e0d44d9fa0f226371c7ede2
Daniel Zoba <daniel.sobe@sorben.com>
2026-04-09 09:09:33 +0200

    add more include paths

--- 0010_compile_dlabpro.sh
+++ 0010_compile_dlabpro.sh
@@ -13,8 +13,9 @@ git checkout 50a0237a2e297ee13e097e928b544a1b8cfc6a7b
 # enable compile fix for newer distros
 # git cherry-pick 297f1dc7a6b86a7ed786f845e93e327576e913ae
 
-# hard-code python3.11 include path
+# hard-code python3.11 include paths
 git cherry-pick c231d0fe9f46959a226cdc6a6829d755a95b4b8f
+git cherry-pick 2f5a93d93c029787bffe642d44877cafebf0be2d
 
 make -j8 -C programs/dlabpro RELEASE
 popd
